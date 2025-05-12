8f3f21a7f81a19ee5cdab7e536fcbc1bc67f6718
dheinzHAK <dheinz@tsn.at>
2025-05-12 12:37:56 +0200

    When branch trafficLightColor

--- Conditionals/app/src/main/java/com/example/conditionals/MainActivity.kt
+++ Conditionals/app/src/main/java/com/example/conditionals/MainActivity.kt
@@ -59,4 +59,16 @@ fun main() {
         println("Invalid traffic-light color")
     }
 
+   /* fun main() {
+        val trafficLightColor = "Amber"
+
+        when (trafficLightColor) {
+            "Red" -> println("Stop")
+            "Yellow", "Amber" -> println("Slow")
+            "Green" -> println("Go")
+            else -> println("Invalid traffic-light color")
+        }
+    }*/
+
 }
+
